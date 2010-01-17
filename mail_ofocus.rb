9b0393a14df91466f9716a574d4f665dbd9f9a9f
steve ulrich <sulrich@botwerks.org>
2010-01-17 10:01:36 -0600

    updated to support improved handling of attachments and multi-part messages

--- mail_ofocus.rb
+++ mail_ofocus.rb
@@ -8,9 +8,33 @@ include Appscript
 of = app('OmniFocus')
 message = STDIN.read
 mail = TMail::Mail.parse(message)
+
+notes = ""
+
+if mail.multipart? then
+  mail.parts.each do |m|
+    if m.content_type == "text/plain"
+      notes << m.body << "\n"
+      notes << "------------------------\n"
+    elsif m.content_type == "multipart/alternative"
+      m.parts.each do |nested|
+    		if nested.content_type == "text/plain"
+          notes << nested.body << "\n"
+        end
+      end
+    else
+      notes << "[non text attachment]\n"
+    end
+  end
+else 
+	notes = mail.body
+end
+
+#puts notes
+
 tasks = of.documents[1].get
 tasks.make(
            :new => :inbox_task, 
            :with_properties =>
-             {:name => mail.subject, :note => mail.body}
+           {:name => mail.subject, :note => notes}
            )
