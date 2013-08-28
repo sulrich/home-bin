68bc2cfc202f507beeea7456356ff152beafad2c
steve ulrich <sulrich@botwerks.org>
2013-08-27 20:56:29 -0500

    misc cleanup to bridge-setup.sh processes

--- bridge-setup.sh
+++ bridge-setup.sh
@@ -1,24 +1,39 @@
 #!/usr//bin/zsh
 
 myname=`basename $0`
-
-TUSER=sulrich
+TUSER=`whoami`
 
 case "$#" in
-0|1|2)    
-        echo "usage: $myname bridge_intf start_tapid end_tapid" 1>&2; 
+0|1|2)
+        echo "usage: $myname bridge_intf start_tapid end_tapid" 1>&2;
         exit 1
         ;;
-*)      bridge_int="$1"; start_tapid="$2" ; end_tapid="$3" 
-        shift; 
+*)      BRIDGE_INTF="$1"; START_TAPID="$2" ; END_TAPID="$3"
+        shift;
         shift
         ;;
 esac
 
-echo "bridge: $bridge_int - start: $start_tapid - end: $end_tapid"
-#exit
+echo " bridge intf: ${BRIDGE_INTF} - start: ${START_TAPID} - end: ${END_TAPID}"
+echo "tunnel owner: ${TUSER}"
 
+/sbin/ifconfig ${BRIDGE_INTF}
+if [ "$?" = "0" ]; then
+  echo "interface ${BRIDGE_INTF} exists -- adding tap interface(s) to ${BRIDGE_INTF}"
+else
+	echo "interface ${BRIDGE_INTF} missing -- creating ...";
+  /usr/bin/sudo ovs-vsctl add-br ${BRIDGE_INTF};
+fi
 
+foreach i (`seq $START_TAPID $END_TAPID`)
+  echo "-- creating interface tap${i}"
+  /usr/bin/sudo tunctl -u ${TUSER} -t tap${i}            ;
+  /usr/bin/sudo ifconfig tap${i} up                      ;
+  echo "-- adding tap${i} to ${BRIDGE_INTF}"
+  /usr/bin/sudo ovs-vsctl add-port ${BRIDGE_INTF} tap${i};
+end
+
+#-------------------------------------------------------------------------------
 # functions
 seq () {
   local lower upper output;
@@ -32,19 +47,3 @@ done;
 
   echo $output
 }
-
-# sudo ovs-vsctl del-br br100
-# sudo ovs-vsctl add-br br100
-
-
-
-
-foreach i (`seq $start_tapid $end_tapid`)
-  /usr/bin/sudo tunctl -u ${TUSER} -t tap${i}     ;
-  /usr/bin/sudo ifconfig tap${i} up              ;
-  /usr/bin/sudo ovs-vsctl add-port $bridge_int tap${i} ; 
-end 
-
-
-
-
