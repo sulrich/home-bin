f56132148483a57dffb33e2691ada2e80436e835
steve ulrich <sulrich@botwerks.org>
2013-12-18 10:40:01 -0600

    moved mirror locations

--- update-mirrors.sh
+++ update-mirrors.sh
@@ -3,5 +3,5 @@
 #rsync --ignore-existing -avz ftp.rfc-editor.org::ids-text-only/  ~/Sites/internet-drafts
 #rsync --ignore-existing -avz www1.ietf.org::internet-drafts/ ~/Sites/internet-drafts
 #rsync -avz www1.ietf.org::internet-drafts/ ~/Sites/internet-drafts
-rsync --ignore-existing -avz --delete ftp.rfc-editor.org::ids-text-only/  ~/mirrors/internet-drafts
-rsync --ignore-existing -avz --delete ftp.rfc-editor.org::rfcs-text-only/ ~/mirrors/rfc
+rsync --ignore-existing -avz --delete ftp.rfc-editor.org::ids-text-only/  ~/src/mirrors/internet-drafts
+rsync --ignore-existing -avz --delete ftp.rfc-editor.org::rfcs-text-only/ ~/src/mirrors/rfc
