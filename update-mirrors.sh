5dfd4535858b94fcad245311ee6a6a89a23c7aa6
steve ulrich <sulrich@botwerks.org>
2014-12-20 13:38:22 -0600

    updated to be run from home server

---
+++ update-mirrors.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+#rsync --ignore-existing -avz ftp.rfc-editor.org::ids-text-only/  ~/Sites/internet-drafts
+#rsync --ignore-existing -avz www1.ietf.org::internet-drafts/ ~/Sites/internet-drafts
+#rsync -avz www1.ietf.org::internet-drafts/ ~/Sites/internet-drafts
+rsync --ignore-existing -avz --delete ftp.rfc-editor.org::ids-text-only/  ~/Dropbox/mirrors/internet-drafts
+rsync --ignore-existing -avz --delete ftp.rfc-editor.org::rfcs-text-only/ ~/Dropbox/mirrors/rfc
