21cc725c8fef47061f9cb4a6aac8662d9667bae8
steve ulrich <sulrich@botwerks.org>
2015-10-08 15:18:25 -0500

    script to sync repos that i'm actively watching
    repo-sync.sh will do a simple git pull for a list of repos.  these repos
    should not be repos that i'm actively doing development or using in projects.

---
+++ repo-sync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# every morning there are repos that i want to make sure I'm aware of updates to.
+# just update this list of repos with the remote contents
+#
+# NOTE - *do not* use this for repos that i'm doing development on myself.
+# these should be kept fresh through the usual channels.
+
+declare -a REPO_LIST=(
+  "${HOME}/src/openconfig/public"
+  "${HOME}/src/openconfig/goyang"
+  "${HOME}/src/yang"
+)
+
+for REPO in "${REPO_LIST[@]}"; do
+  echo "updating repo: ${REPO}"
+  echo "----------------------------------------------------------------------"
+  cd ${REPO}
+  git pull
+done
