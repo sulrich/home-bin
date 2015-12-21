ba348b7b771d34fc7f6949d18224dcbb425654fc
steve ulrich <sulrich@botwerks.org>
2015-12-21 01:05:17 -0600

    misc. cleanup for globbing

---
+++ nuke.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# when you drop the mic
+
+
+declare -a PERSONAL=( 
+  "${HOME}/.gnupg"                # personal GPG keys 
+  "${HOME}/.config"               # personal misc. config files
+  "${HOME}/.ssh"                  # personal ssh config
+  "${HOME}/.jupyter"              # 
+  "${HOME}/.dropbox"              # 
+  "${HOME}/.ipython"              # 
+  "${HOME}/.gist-vim"             # 
+  "${HOME}/.gitconfig"            # 
+  "${HOME}/.getmail"              # 
+  "${HOME}/.vagrant.d"            # 
+  "${HOME}/.keybase-installer"    # 
+  "${HOME}/.notes"                # 
+  "${HOME}/.splunk"               # 
+
+  "${HOME}/.Rhistory"             # 
+  "${HOME}/.bash_history"         # 
+  "${HOME}/.zsh_history"          # 
+
+  # ~/Library stuff
+  "${HOME}/Library/Messages"    # apple messages cache
+  "${HOME}/Library/Calendars"   # 
+  "${HOME}/Library/Cookies"     #
+  "${HOME}/Library/Application Support/1Password 4"   # 
+  "${HOME}/Library/Application Support/AddressBook"   # 
+  "${HOME}/Library/Application Support/Evernote"      # 
+
+  # misc. directories, etc.
+  "${HOME}/Documents/Wolfram Mathematica"   
+  "${HOME}/Documents/Colloquy Transcripts"  
+  "${HOME}/Documents/Microsoft User Data"   
+  "${HOME}/Documents/My Tableau Repository" 
+  "${HOME}/src/customer-configs"      
+  "${HOME}/tmp"                      
+
+  "${HOME}/.home"              # my dotfiles!
+
+) # end of PERSONAL directories list
+
+
+for D in "${PERSONAL[@]}": 
+  do 
+    echo "removing: $D"
+    rm -rf "${D}"
+
+done
+
+
+echo "cleaning out misc dreck"
+echo "cleaning out: ~/Downloads dir"
+rm -rf "${HOME}/Downloads/*"
+echo "cleaning out: ~/Music dir"
+rm -rf "${HOME}/Music/*"
+
+
+
+
+echo "clear safari history, etc."
+echo "clear chome history, etc."
+echo "clear chome user accounts"
+echo "remember to remove the itunes authorization for this host"
+echo "remember to whack your 3rd party accounts (twitter, facebook, etc.)"
+echo "remember to whack your ~/bin directory as well ..."
+
--- roll-mail.sh
+++ roll-mail.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+# -*- mode: sh; fill-column: 78; comment-column: 50; tab-width: 2 -*-
 
 # roll the current directory full of maildirs over to the archives.  this makes
 # some assumptions as to the directory layout as specified in the $ARCH_DIR
 # variable
 
-LAST_MO_YEAR=`date -v -1m +%Y`
-SRC_DIR=`basename ${PWD}`
+LAST_MO_YEAR=$(date -v -1m +%Y)
+SRC_DIR=$(basename "${PWD}")
 ARCH_DIR="${HOME}/Documents/work/archives/mail/${LAST_MO_YEAR}/${SRC_DIR}"
 
 
 # foreach m ( ${PWD}/* ) echo $m; end
 echo "rolling over ${PWD} -> $ARCH_DIR"
 
-for m in  `basename ${PWD}/*` ; do
-    ${HOME}/bin/split-maildirs.pl --keep_recent \
-        --arch_dir=${ARCH_DIR}   --src_dir=${PWD} $m;
-    echo $m;
+for M in  $(basename "${PWD}/*") ; do
+  ${HOME}/bin/split-maildirs.pl --keep_recent \
+         --arch_dir="${ARCH_DIR}" --src_dir="${PWD}" "${M}";
+  echo "${M}";
 done
