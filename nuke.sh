78789122358331c942c29ed38bef4bc1b8a632d5
steve ulrich <sulrich@botwerks.org>
2016-02-27 09:30:12 -0600

    updates to nuke script

--- nuke.sh
+++ nuke.sh
@@ -1,56 +1,64 @@
 #!/bin/bash
 
-# when you drop the mic
+# for when you drop the mic
 
-
-declare -a PERSONAL=( 
-  "${HOME}/.gnupg"                # personal GPG keys 
+declare -a PERSONAL=(
+  "${HOME}/.gnupg"                # personal GPG keys
   "${HOME}/.config"               # personal misc. config files
   "${HOME}/.ssh"                  # personal ssh config
-  "${HOME}/.jupyter"              # 
-  "${HOME}/.dropbox"              # 
-  "${HOME}/.ipython"              # 
-  "${HOME}/.gist-vim"             # 
+  "${HOME}/.jupyter"              #
+  "${HOME}/.dropbox"              #
+  "${HOME}/.ipython"              #
+  "${HOME}/.gist-vim"             #
   "${HOME}/.gitconfig"            # has github credentials too
-  "${HOME}/.getmail"              # 
-  "${HOME}/.vagrant.d"            # 
-  "${HOME}/.keybase-installer"    # 
-  "${HOME}/.notes"                # 
-  "${HOME}/.splunk"               # 
+  "${HOME}/.getmail"              #
+  "${HOME}/.vagrant.d"            #
+  "${HOME}/.keybase-installer"    #
+  "${HOME}/.notes"                #
+  "${HOME}/.splunk"               #
+  "${HOME}/.gnupg"                #
+  "${HOME}/.docker"               #
+  "${HOME}/.cpanm"                #
+  "${HOME}/.lesshst"              #
+  "${HOME}/.links"                #
+  "${HOME}/.local"                #
+  "${HOME}/.mairixdb"             #
+  "${HOME}/.mairixrc"             #
 
-  "${HOME}/.Rhistory"             # 
-  "${HOME}/.bash_history"         # 
-  "${HOME}/.zsh_history"          # 
+  "${HOME}/.Rhistory"             #
+  "${HOME}/.bash_history"         #
+  "${HOME}/.zsh_history"          #
+
 
   # ~/Library stuff
   "${HOME}/Library/Messages"    # apple messages cache
-  "${HOME}/Library/Calendars"   # 
+  "${HOME}/Library/Calendars"   #
   "${HOME}/Library/Cookies"     #
-  "${HOME}/Library/Application Support/1Password 4"   # 
-  "${HOME}/Library/Application Support/AddressBook"   # 
-  "${HOME}/Library/Application Support/Evernote"      # 
+  "${HOME}/Library/Application Support/1Password 4"   #
+  "${HOME}/Library/Application Support/AddressBook"   #
+  "${HOME}/Library/Application Support/Evernote"      #
 
   # containers
-  "${HOME}/Library/Containers/com.dayoneapp.dayone"              # 
-  "${HOME}/Library/Containers/com.omnigroup.OmniFocus2"          # 
-  "${HOME}/Library/Containers/com.flexibits.fantastical2.mac*"   # 
+  "${HOME}/Library/Containers/com.dayoneapp.dayone"              #
+  "${HOME}/Library/Containers/com.omnigroup.OmniFocus2"          #
+  "${HOME}/Library/Containers/com.flexibits.fantastical2.mac*"   #
 
 
   # misc. directories, etc.
-  "${HOME}/Documents/Wolfram Mathematica"   
-  "${HOME}/Documents/Colloquy Transcripts"  
-  "${HOME}/Documents/Microsoft User Data"   
-  "${HOME}/Documents/My Tableau Repository" 
-  "${HOME}/src/customer-configs"      
-  "${HOME}/tmp"                      
+  "${HOME}/Documents/Wolfram Mathematica"
+  "${HOME}/Documents/Colloquy Transcripts"
+  "${HOME}/Documents/Microsoft User Data"
+  "${HOME}/Documents/My Tableau Repository"
+  "${HOME}/src/customer-configs"
+  "${HOME}/tmp"
 
   "${HOME}/.home"              # my dotfiles!
 
 ) # end of PERSONAL directories list
 
 
-for D in "${PERSONAL[@]}": 
-  do 
+for D in "${PERSONAL[@]}":
+  do
     echo "removing: $D"
     # rm -rf "${D}"
 
@@ -73,4 +81,4 @@ echo "remember to unlink this computer from dropbox"
 echo "remember to remove the itunes authorization for this host"
 echo "remember to whack your 3rd party accounts (twitter, facebook, etc.)"
 echo "remember to whack your ~/bin directory as well ..."
-
+echo "remove jetdrive"
