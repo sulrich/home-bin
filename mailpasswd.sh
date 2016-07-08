b676dcd3c6184bf45a26cc8187d25cd2a689c336
steve ulrich <sulrich@botwerks.org>
2016-07-08 09:00:13 -0500

    added mailpasswd.sh

---
+++ mailpasswd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+MUTT_STR="set smtp_pass=\"%%PASSWORD%%\"     #SMTP password"
+IMAP_STR="%%PASSWORD%%"
+
+CREDS_DIR="${HOME}/.credentials"
+MUTT_FILE="${CREDS_DIR}/mutt-passwords.gpg"
+IMAP_FILE="${CREDS_DIR}/offlineimap-passwd.gpg"
+
+echo -n "enter new password: "
+read PASSWORD
+
+echo ${MUTT_STR} | sed "s/%%PASSWORD%%/${PASSWORD}/" | gpg2 -e \
+       -r sulrich@juniper.net -o ${MUTT_FILE}
+echo ${IMAP_STR} | sed "s/%%PASSWORD%%/${PASSWORD}/" | gpg2 -e \
+       -r sulrich@juniper.net -o ${IMAP_FILE}
+
