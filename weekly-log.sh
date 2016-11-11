7bd7db1560414d29e763940e9b4678b5848c244a
steve ulrich <sulrich@botwerks.org>
2016-11-11 13:33:54 -0600

    updated weekly log script to work from any day of the week
    fixed the need for this to be run via cron on specific days. can also be
    run from the command line with the right date calculations in place.

--- weekly-log.sh
+++ weekly-log.sh
@@ -6,8 +6,10 @@
 DESKTOP_FILE="${HOME}/Desktop/worklog-current.org"
 HEAD_TEMPLATE="${HOME}/.notes/juniper/weekly-notes/templates/head.org"
 LOG_DIR="${HOME}/.notes/juniper/weekly-notes"
-LAST_WEEK=$(date -v-1w +"%Y%m%d")
-CURRENT_WEEK=$(date +"%Y%m%d")
+# NB: this date stuff only works on the mac
+CURRENT_WEEK=$(date -v "-Mon" +"%Y%m%d")
+# you can chain these date calcs together - who knew?
+LAST_WEEK=$(date -v "-Mon" -v-1w +"%Y%m%d")
 YEAR=$(date +"%Y")
 
 rm ${DESKTOP_FILE}
