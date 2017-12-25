bd864bee8db4e947b81a434b40ae2b23efa6005c
steve ulrich <sulrich@botwerks.org>
2017-12-25 17:12:05 -0600

    misc. shellcheck cleanup

--- backup.sh
+++ backup.sh
@@ -13,7 +13,8 @@ declare -a DOTFILES=("gnupg" "ssh" "aws" "ipython" "matplotlib" "perlbrew"
 # RETAIN THE TRAILING SLASH ON THE SOURCE
 
 USB_DRIVE="TeraBacktyl"  # the name of the local backup USB drive to use
-RSYNC_OPTS="-avuzHSq --delete-after"
+# this should be an array to provide the right arg processing later. 
+declare -a RSYNC_OPTS=("-avuzHSq" "--delete-after")
 OPTIND=1         # reset in case getopts has been used previously in the script
 
 # display usage info
@@ -44,48 +45,55 @@ do
     exit 0
     ;;
   l)
-    RPATH=("${RPATH[@]}" "/Volumes/${USB_DRIVE}/jnpr-backup"
+    # RPATH=("${RPATH[@]}" "/Volumes/${USB_DRIVE}/jnpr-backup"
+    RPATH=( "/Volumes/${USB_DRIVE}/jnpr-backup"
            "sulrich@bert.local.:/mnt/snuffles/home/sulrich/jnpr-backup")
     echo "-- local backup"
-    echo "-- path: ${RPATH[@]}"
+    echo "-- path: ${RPATH[*]}"
     ;;
   r)
-    RPATH=("${RPATH[@]}"
-           "sulrich@dyn.botwerks.net:/mnt/snuffles/home/sulrich/jnpr-backup")
+    RPATH=("sulrich@dyn.botwerks.net:/mnt/snuffles/home/sulrich/jnpr-backup")
     echo "-- remote backup"
-    echo "path: ${RPATH[@]}"
+    echo "-- path: ${RPATH[*]}"
     ;;
   v)
     echo "-- verbose mode"
-    # rip out the -q flag - -q suppresses non-ERROR output
-    RSYNC_OPTS=$(echo ${RSYNC_OPTS} | sed 's/q//')
+    # rip out the -q flag, -q suppresses non-ERROR output
+    declare -a RSYNC_OPTS=( "${RSYNC_OPTS[@]/q/}" )
     ;;
+  *)
+    # echo usage
+    echo "specify remote or locale execution"
+    show_usage
+    exit 1
   esac
 done
 
 shift "$((OPTIND-1))"
-[ "$1" = "--" ] && shift
+[ "$1" = "--" ] &p& shift
 
 # get the latest list of ~/ symlinks
 echo "snapshot symlinks"
-ls -la ${HOME} > ${HOME}/Dropbox/personal/configs/homedir-ls.txt
+ls -la "${HOME}" > "${HOME}/Dropbox/personal/configs/homedir-ls.txt"
 # update installed brew apps list
 echo "backing up brew list"
-brew list       > ${HOME}/Dropbox/personal/configs/brew-list.txt
-brew cask list >> ${HOME}/Dropbox/personal/configs/brew-list.txt
+brew list       > "${HOME}/Dropbox/personal/configs/brew-list.txt"
+brew cask list >> "${HOME}/Dropbox/personal/configs/brew-list.txt"
 # dump my crontab
 echo "backing up crontab"
-crontab -l > ${HOME}/Dropbox/personal/configs/crontab
+crontab -l > "${HOME}/Dropbox/personal/configs/crontab"
 
 
-echo "rsync flags: ${RSYNC_OPTS}"
+echo "rsync flags: ${RSYNC_OPTS[*]}"
 for R in "${RPATH[@]}"
 do
   echo "backing up to ${R}"
   echo "------------------------------------------------------------"
-  /usr/bin/rsync ${RSYNC_OPTS}                                         \
-                 --exclude-from="${HOME}/bin/backup-exclude-list.txt"  \
-                 "${HOME}/"  "${R}"
+  # note that the RSYNC_OPTS below should _not_ be doublequoted, rsync barfs on
+  # that.
+  /usr/bin/rsync ${RSYNC_OPTS[*]}                                     \
+                 --exclude-from="${HOME}/bin/backup-exclude-list.txt" \
+                   "${HOME}/"  "${R}"
 done
 
 ## local >> jetdrive
@@ -96,5 +104,5 @@ echo "------------------------------------------------------------"
 for L in "${DOTFILES[@]}"
 do
   echo  "- ${L}"
-  /usr/bin/rsync ${RSYNC_OPTS} "${HOME}/.${L}/"  "${JDEST}/${L}"
+  /usr/bin/rsync ${RSYNC_OPTS[*]} "${HOME}/.${L}/"  "${JDEST}/${L}"
 done
