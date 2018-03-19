030be1f30313e5d6e444766a5bb4fd34b7ad0051
steve ulrich <sulrich@botwerks.org>
2018-03-19 06:12:48 -0500

    misc. ssh to hom cleanup

---
+++ ssh-home.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# script to provide ssh tunnel to internal hosts in a single shot with the
+# associated port mappings to access internal web interfaces.
+#
+# list of internal hosts with mappings:
+#
+# 10.0.0.241 - virl server
+# 10.0.0.243 - snuffles admin interface
+# 10.0.0.245 - cmonster vnc instance
+# 10.0.0.251 - ubiquity web management console
+#
+
+ssh -L 8080:10.0.0.241:80   \
+    -L 5000:10.0.0.243:5000 \
+    -L 6901:10.0.0.245:5901 \
+    -L 8443:10.0.0.251:8443 \
+    dyn.botwerks.net
