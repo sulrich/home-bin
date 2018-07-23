66a73ef4e098fa57b76988ae45f6c9410ebec451
steve ulrich <sulrich@botwerks.org>
2018-07-22 21:33:21 -0500

    added application list backup

--- backup.sh
+++ backup.sh
@@ -85,7 +85,10 @@ brew cask list >> "${HOME}/Dropbox/personal/configs/brew-list.txt"
 # dump my crontab
 echo "backing up crontab"
 crontab -l > "${HOME}/Dropbox/personal/configs/crontab"
-
+# echo list /Applications
+echo "capturing installed apps"
+ls -1 /Applications > "${HOME}/Dropbox/personal/configs/apps-list.txt"
+ls -1 "${HOME}/Applications " >> "${HOME}/Dropbox/personal/configs/apps-list.txt"
 
 echo "rsync flags: ${RSYNC_OPTS[*]}"
 for R in "${RPATH[@]}"
