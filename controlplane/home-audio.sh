5c565f7417d45985d79f6197f8d48cca172ffd5b
steve ulrich <sulrich@botwerks.org>
2018-08-27 11:53:23 -0500

    misc. notes for control-plane scripts

--- controlplane/home-audio.sh
+++ controlplane/home-audio.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# this requires the use of the switchaudio-osx command line
+# application.  this can be found at the following location:
+#
+# https://github.com/deweller/switchaudio-osx/
+#
+# also available via brew: brew install switchaudio-osx
+#
 
 # set audio device to be MM_1 speakers
 /usr/local/bin/SwitchAudioSource -s "MM-1"
