787da44e65a43dd8cdbfa130c0db75d2f9e41fdf
steve ulrich <sulrich@botwerks.org>
2020-04-21 10:04:42 -0500

    toggle-wemo.sh cleanup
    adjusted to use pyenv in cron environment
    less chatty

--- toggle-wemo.sh
+++ toggle-wemo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+export PATH="${HOME}/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+
 # toggle the power on the wemo bridge.  
 WEMO_PORT=4
 
 ${HOME}/bin/botpower.py -a off -o ${WEMO_PORT}
-echo "pausing 10 seconds ..."
 sleep 10
-echo "restarting wemo bridge"
 ${HOME}/bin/botpower.py -a on  -o ${WEMO_PORT}
