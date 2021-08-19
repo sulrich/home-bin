99e727146998ff208542335fc0c6c1976f4761b0
steve ulrich <sulrich@botwerks.org>
2021-08-19 08:45:26 -0500

    cert-renew.sh updates

--- cert-renew.sh
+++ cert-renew.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#export WEBROOT="/home/shared/www/botwerks.net"
+# run certbot with the renew option to make sure that we're never in a spot
+# where we lose our certs and cause things to get borked.
 # 
-# /home/sulrich/certbot/certbot-auto renew \
-#   -a webroot --webroot-path=${WEBROOT}
+# put this some place root can access it and add it to the root crontab with
+# the following entry
+# 
+# m h  dom mon dow   command
+# 23  15  *   *   *   /...path_to.../cert-renew.sh
 
-/usr/bin/letsencrypt renew
+/usr/bin/certbot renew
 systemctl restart nginx 
