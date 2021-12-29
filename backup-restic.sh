879e05e0a877a18ac8cc8f71c467cab23ede0d16
steve ulrich <sulrich@botwerks.org>
2021-12-29 10:38:14 -0600

    misc. improvements to backup-restic.sh

--- backup-restic.sh
+++ backup-restic.sh
@@ -2,7 +2,8 @@
 
 # get the password for the restic repo.
 RESTIC_PASSWORD_COMMAND="/usr/local/bin/op get item ${SULRICH_BKUP_1P} --fields password"
-BREWFILE="${HOME}/iCloud/src/configs/${HOST}/Brewfile.txt"
+BREWFILE="${HOME}/iCloud/src/configs/${HOSTNAME}/Brewfile.txt"
+BREWFILE_PREV="${HOME}/iCloud/src/configs/${HOSTNAME}/brewfile-prev.txt"
 # reset in case getopts has been used previously in the script
 OPTIND=1         
 
@@ -18,6 +19,7 @@ usage: ${0##*/} [-hlrv]
 
 IMPORTANT NOTE
 the following environment variables must be set 
+- SULRICH_BKUP_1P - the name of the 1password key to use for this host
 - SULRICH_BKUP_RPATH - defines remote backup path
 - SULRICH_BKUP_EXCLUDE - path to the host specific rsync exclusion file 
 
@@ -78,13 +80,14 @@ ls -la "${HOME}" > "${HOME}/iCloud/src/configs/${HOSTNAME}/homedir-ls.txt"
 echo "backing up brew list"
 brew list --formula > "${HOME}/iCloud/src/configs/${HOSTNAME}/brew-list.txt"
 brew list --cask    > "${HOME}/iCloud/src/configs/${HOSTNAME}/brew-cask-list.txt"
+echo "moving old Brewfile"
+mv "${BREWFILE}" "${BREWFILE_PREV}"
+echo "dumping Brewfile"
 brew bundle dump --file="${BREWFILE}"
-# dump my crontab
 echo "backing up crontab"
 crontab -l > "${HOME}/iCloud/src/configs/${HOSTNAME}/crontab"
-echo "backing up ssh/config"
-cp "${HOME}/.ssh/config" "${HOME}/iCloud/src/configs/${HOSTNAME}-ssh-config"
-# echo list /Applications
+echo "backing up ${HOME}/.ssh/config"
+cp "${HOME}/.ssh/config" "${HOME}/iCloud/src/configs/${HOSTNAME}/ssh-config"
 echo "capturing installed apps"
 ls -1 "/Applications"         > "${HOME}/iCloud/src/configs/${HOSTNAME}/app-list.txt"
 ls -1 "${HOME}/Applications" >> "${HOME}/iCloud/src/configs/${HOSTNAME}/app-list.txt"
