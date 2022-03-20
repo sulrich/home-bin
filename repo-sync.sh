5a5b7b7db6bef21700066f9f9d9998b61bc437e0
steve ulrich <sulrich@botwerks.org>
2022-03-20 13:33:34 -0500

    updated repo-sync to handle upstream repos

--- repo-sync.sh
+++ repo-sync.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+## -*- mode: sh; fill-column: 78; comment-column: 50; tab-width: 2 -*-
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+
+usage() {
+    cat << EOF
+usage: ${0##*/} [-h]
+
+    -h          display help and exit
+    XXX - list of args here
+
+EOF
+}
+
+# anything that has ## at the front of the line will be used as input.
+## help: details the available functions in this script
+help() {
+  usage
+  echo "available functions:"
+  sed -n 's/^##//p' $0 | column -t -s ':' | sed -e 's/^/ /'
+}
+
+cleanup() {
+    trap - SIGINT SIGTERM ERR EXIT
+    # script cleanup here, tmp files, etc.
+}
 
 # every morning there are repos that i want to make sure I'm aware of updates to.
 # just update this list of repos with the remote contents
@@ -8,6 +35,38 @@
 
 HOSTNAME=$(hostname)
 source "${HOME}/.home/repo-lists/${HOSTNAME}"
+
+
+# misc. git functions
+function git-upstream-sync() {
+  # for stuff that i am actively working on with others, work off of my fork and
+  # update my $default_branch with the contents of the upstream. 
+
+  # check for pending changes and break out of the update, this should be
+  # resolved and updated manually
+  git diff-index --quiet HEAD --
+  if [ $? -eq 1 ]; then
+    echo "ERROR: uncommitted changes ($(pwd))"
+    cleanup
+    exit 1
+  fi
+
+  # determine if the repo uses master/main as the default branch name
+  local DEFAULT_BRANCH=$(git remote show upstream | grep 'HEAD branch' | awk '{print $NF}')
+  echo "default branch: ${DEFAULT_BRANCH}"
+
+  if [ -z "${DEFAULT_BRANCH}" ]; then
+    echo ""
+    echo "the upstream repo is undefined, you will need to add an upstream repo to track"
+    echo "git remote add upstream <upstream-url-here>"
+    echo ""
+  else
+    git checkout "${DEFAULT_BRANCH}"
+    git fetch upstream
+    git pull upstream "${DEFAULT_BRANCH}"
+    git push origin "${DEFAULT_BRANCH}"
+  fi
+}
 
 for REPO in "${REPO_LIST[@]}"; do
   echo "updating repo: ${REPO}"
@@ -28,3 +87,11 @@ for REPO in "${MIRROR_LIST[@]}"; do
   git push "${INTERNAL_REMOTE}"
   echo ""
 done
+
+for REPO in "${UPSTREAM_LIST[@]}"; do
+  echo "syncing to upstream: ${REPO}"
+  cd "${REPO}" || exit
+  git-upstream-sync 
+done
+
+cleanup
