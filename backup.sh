8b6bc78f49ed06d4ae862b7a2572f934d0f0083a
steve ulrich <sulrich@botwerks.org>
2022-06-27 10:20:07 -0500

    backup-*.sh cleanup
    i've created too may variations here over the years.

--- backup.sh
+++ backup.sh
@@ -1,44 +1,37 @@
 #!/bin/bash
 
-DTS=$(date +"%Y%m%d-%H%M")
-BACKUP_LOG_FILE="/tmp/backup-${DTS}.log"
+# get the password for the restic repo.
+RESTIC_PASSWORD_COMMAND="/usr/local/bin/op item get ${SULRICH_BKUP_1P} --fields password"
+BREWFILE="${HOME}/iCloud/src/configs/${HOSTNAME}/Brewfile.txt"
+BREWFILE_PREV="${HOME}/iCloud/src/configs/${HOSTNAME}/brewfile-prev.txt"
+# reset in case getopts has been used previously in the script
+OPTIND=1         
 
-BREWFILE="${HOME}/iCloud/src/configs/${HOST}/Brewfile.txt"
-
-# the following ARGS must be an array to provide the right arg processing later.
-declare -a RSYNC_OPTS=("-avuzHSq" "--delete-after" "--log-file=${BACKUP_LOG_FILE}")
-OPTIND=1         # reset in case getopts has been used previously in the script
-
-HOST=$(hostname)
-
-# display usage info
 show_usage() {
   cat << EOF
 usage: ${0##*/} [-hlrv]
 
     -h          display help and exit
-    -l          local backup (direct attached drive) and remote (over ssh) 
-                ssh backup will used bonjour appropriate hostname(s).
-    -r          remote backup only (over ssh)
+    -l          local LAN backup (over ssh)
+    -r          remote backup only (over 
     -v          verbose mode
 
 IMPORTANT NOTE
 the following environment variables must be set 
+- SULRICH_BKUP_1P - the name of the 1password key to use for this host
 - SULRICH_BKUP_RPATH - defines remote backup path
 - SULRICH_BKUP_EXCLUDE - path to the host specific rsync exclusion file 
 
-these are documented in the accompanyting zshenv files.
+these are documented in the zshenv files.
 EOF
 }
 
-
-if [ -z "${SULRICH_BKUP_RPATH}" ] || [ -z "${SULRICH_BKUP_EXCLUDE}"  ];
+if [ -z "${SULRICH_BKUP_RPATH}" ] || [ -z "${SULRICH_BKUP_EXCLUDE}" ];
 then
   echo "required environment vars missing!"
   show_usage
   exit 1
 fi
-
 
 if [ $# -eq 0 ];
 then
@@ -47,7 +40,7 @@ then
   exit 1
 fi
 
-while getopts "hlrv" OPT;
+while getopts "hlr" OPT;
 do
   case "${OPT}" in
   h)
@@ -55,23 +48,20 @@ do
     exit 0
     ;;
   l)
-    RPATH=("sulrich@abby.local.:${SULRICH_BKUP_RPATH}")
     echo "-- local backup"
-    echo "-- path: ${RPATH[*]}"
+    BKUP_HOST="snuffles"
+    echo "-- host: ${BKUP_HOST}"
+    echo "-- path: ${SULRICH_BKUP_RPATH}"
     ;;
   r)
-    RPATH=("sulrich@dyn.botwerks.net:${SULRICH_BKUP_RPATH}")
     echo "-- remote backup"
-    echo "-- path: ${RPATH[*]}"
-    ;;
-  v)
-    echo "-- verbose mode"
-    # rip out the -q flag, -q suppresses non-ERROR output
-    declare -a RSYNC_OPTS=( "${RSYNC_OPTS[@]/q/}" )
+    BKUP_HOST="snuffles-remote"
+    echo "-- host: ${BKUP_HOST}"
+    echo "-- path: ${SULRICH_BKUP_RPATH}"
     ;;
   *)
     # echo usage
-    echo "specify remote or locale execution"
+    echo "specify remote or local execution"
     show_usage
     exit 1
   esac
@@ -80,42 +70,28 @@ done
 shift "$((OPTIND-1))"
 [ "$1" = "--" ] && shift
 
+RPATH="sftp:${USER}@${BKUP_HOST}:${SULRICH_BKUP_RPATH}"
+
 # get the latest list of ~/ symlinks
 echo "snapshot symlinks"
-ls -la "${HOME}" > "${HOME}/iCloud/src/configs/${HOST}/homedir-ls.txt"
+ls -la "${HOME}" > "${HOME}/iCloud/src/configs/${HOSTNAME}/homedir-ls.txt"
 # update installed brew apps list
 echo "backing up brew list"
-brew list --formula > "${HOME}/iCloud/src/configs/${HOST}/brew-list.txt"
-brew list --cask    > "${HOME}/iCloud/src/configs/${HOST}/brew-cask-list.txt"
+brew list --formula > "${HOME}/iCloud/src/configs/${HOSTNAME}/brew-list.txt"
+brew list --cask    > "${HOME}/iCloud/src/configs/${HOSTNAME}/brew-cask-list.txt"
+echo "moving old Brewfile"
+mv "${BREWFILE}" "${BREWFILE_PREV}"
+echo "dumping Brewfile"
 brew bundle dump --file="${BREWFILE}"
-# dump my crontab
 echo "backing up crontab"
-crontab -l > "${HOME}/iCloud/src/configs/${HOST}/crontab"
-# echo list /Applications
+crontab -l > "${HOME}/iCloud/src/configs/${HOSTNAME}/crontab"
+echo "backing up ${HOME}/.ssh/config"
+cp "${HOME}/.ssh/config" "${HOME}/iCloud/src/configs/${HOSTNAME}/ssh-config"
 echo "capturing installed apps"
-ls -1 "/Applications"         > "${HOME}/iCloud/src/configs/${HOST}/app-list.txt"
-ls -1 "${HOME}/Applications" >> "${HOME}/iCloud/src/configs/${HOST}/app-list.txt"
+ls -1 "/Applications"         > "${HOME}/iCloud/src/configs/${HOSTNAME}/app-list.txt"
+ls -1 "${HOME}/Applications" >> "${HOME}/iCloud/src/configs/${HOSTNAME}/app-list.txt"
 
-echo "rsync flags: ${RSYNC_OPTS[*]}"
-for R in "${RPATH[@]}"
-do
-  echo "backup dst: ${R}"
-  echo " exclusion: ${SULRICH_BKUP_EXCLUDE}"
-  echo "------------------------------------------------------------"
-  # note that the RSYNC_OPTS below should _not_ be doublequoted, rsync barfs on
-  # that.
-  /usr/bin/rsync ${RSYNC_OPTS[*]}                         \
-                 --exclude-from="${SULRICH_BKUP_EXCLUDE}" \
-                   "${HOME}/"  "${R}"
-done
-
-# ## local >> jetdrive
-# JDEST="/Volumes/JetDrive/local_mirror"
-# echo "local dotfile backups"
-# echo "------------------------------------------------------------"
-#
-# for L in "${DOTFILES[@]}"
-# do
-#   echo  "- ${L}"
-#   /usr/bin/rsync ${RSYNC_OPTS[*]} "${HOME}/.${L}/"  "${JDEST}/${L}"
-# done
+restic -r "${RPATH}"                              \
+  --exclude-file="${SULRICH_BKUP_EXCLUDE}"        \
+  --password-command="${RESTIC_PASSWORD_COMMAND}" \
+  backup ~/
