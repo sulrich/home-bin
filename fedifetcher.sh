ce5f59881c52aaae752d718eb6721a9c5361394d
steve ulrich <sulrich@botwerks.org>
2023-11-03 17:42:17 -0500

    fixed the fedifetcher.sh script

--- fedifetcher.sh
+++ fedifetcher.sh
@@ -17,7 +17,7 @@ source "${HOME}/.credentials/fedifetcher.txt"
 (flock -n 9 || exit 1;
 
   # ... commands executed under lock ...; 
-  docker run                         \
+  docker run --rm                    \
     -v ${LOCAL_CACHE}:/app/artifacts \
     ghcr.io/nanos/fedifetcher:latest \
     --access-token="${ACCESS_TOKEN}" \
