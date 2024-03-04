13f7b0bda6dc6b0cea47ed60d34d34f9da930844
steve ulrich <sulrich@botwerks.org>
2024-03-04 11:40:14 -0600

    removed -it from mastodon-cleanup.sh
    turns out that don't play with cron so nice.

--- mastodon-cleanup.sh
+++ mastodon-cleanup.sh
@@ -4,8 +4,8 @@
 #
 # the container image is specific to this installation.
 
-/usr/bin/docker exec -it mstdn-web tootctl media usage
-/usr/bin/docker exec -it mstdn-web tootctl media remove --days=30
-/usr/bin/docker exec -it mstdn-web tootctl preview_cards remove --days=30
-/usr/bin/docker exec -it mstdn-web tootctl media usage
+/usr/bin/docker exec mstdn-web tootctl media usage
+/usr/bin/docker exec mstdn-web tootctl media remove --days=30
+/usr/bin/docker exec mstdn-web tootctl preview_cards remove --days=30
+/usr/bin/docker exec mstdn-web tootctl media usage
 
