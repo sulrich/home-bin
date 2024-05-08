f7029ad2061aa33269994b906bec87c2cfb8062a
steve ulrich <sulrich@botwerks.org>
2024-05-08 12:14:06 -0500

    updated daily-notes.sh script

--- daily-notes.sh
+++ daily-notes.sh
@@ -5,17 +5,18 @@ export PATH="/opt/homebrew/bin:$PATH"
 
 # feed the ia writer machine with the reasonable template
 NOTE_DIR="${HOME}/.notes"
+YEAR=$(date +"%Y")
 TODAY=$(date +"%Y%m%d")
 CREATE_DATE=$(date +"%Y-%m-%d")
 NOTE_TEMPLATE="${HOME}/.home/templates/markdown/daily-notes.md"
 
 PERSONAL_CLASS="personal"
 PERSONAL_EMAIL="sulrich@botwerks.org"
-PERSONAL_FILE="${NOTE_DIR}/${TODAY}-personal-notes.md"
+PERSONAL_FILE="${NOTE_DIR}/${YEAR}/${TODAY}-personal-notes.md"
 PERSONAL_TAGS="#personal #diary #notes"
 WORK_CLASS="work"
 WORK_EMAIL="sulrich@arista.com"
-WORK_FILE="${NOTE_DIR}/${TODAY}-work-notes.md"
+WORK_FILE="${NOTE_DIR}/${YEAR}/${TODAY}-work-notes.md"
 WORK_TAGS="#arista #work"
 
 get_weather() {
@@ -27,30 +28,27 @@ get_weather() {
   LOCATION="${CITY}, ${STATE}"
   CITY=$(echo ${LOCATION} | tr -d ' ')
 
-  WEATHER=$(curl -s "http://wttr.in/${CITY}?format=+%c(%C)+%t(%f)+")
+  # WEATHER=$(curl -s "http://wttr.in/${CITY}?format=+%c(%C)+%t(%f)+")
+  WEATHER="fill this in yourself steve!"
   LOCATION=$(echo "${LOCATION}" | tr '[:upper:]' '[:lower:]')
 }
 
 ## work: emits the work-day notes template
 write_work() {
   sed "s/%%TODAY%%/${TODAY}/" < "${NOTE_TEMPLATE}" |\
-      sed "s/%%WEATHER%%/${WEATHER}/g"             |\
       sed "s/%%CREATE_DATE%%/${CREATE_DATE}/g"     |\
       sed "s/%%TAGS%%/${WORK_TAGS}/g"              |\
       sed "s/%%CLASS%%/${WORK_CLASS}/g"            |\
-      sed "s/%%EMAIL%%/${WORK_EMAIL}/g"            |\
-      sed "s/%%LOCATION%%/${LOCATION}/g" >> "${WORK_FILE}"
+      sed "s/%%EMAIL%%/${WORK_EMAIL}/g" >> "${WORK_FILE}"
 }
 
 ## personal: emits the personal notes template
 write_personal(){
   sed "s/%%TODAY%%/${TODAY}/" < "${NOTE_TEMPLATE}" |\
-      sed "s/%%WEATHER%%/${WEATHER}/g"             |\
       sed "s/%%CREATE_DATE%%/${CREATE_DATE}/g"     |\
       sed "s/%%TAGS%%/${PERSONAL_TAGS}/g"          |\
       sed "s/%%CLASS%%/${PERSONAL_CLASS}/g"        |\
-      sed "s/%%EMAIL%%/${PERSONAL_EMAIL}/g"        |\
-      sed "s/%%LOCATION%%/${LOCATION}/g" >> "${PERSONAL_FILE}"
+      sed "s/%%EMAIL%%/${PERSONAL_EMAIL}/g" >> "${PERSONAL_FILE}"
 }
 
 
@@ -81,17 +79,14 @@ fi
 
 case $1 in
   personal)
-    get_weather
     write_personal
     exit
     ;;
   work)
-    get_weather
     write_work
     exit
     ;;
   both)
-    get_weather
     write_both
     exit
     ;;
