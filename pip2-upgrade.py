4ca62b6e775d7cee9b3e2ea558200b93da72292b
steve ulrich <sulrich@botwerks.org>
2024-11-12 08:35:36 -0600

    added dependency injection for get-anet-eox.py

--- pip2-upgrade.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2.7
-
-from pip._internal.utils.misc import get_installed_distributions
-# import pip
-from subprocess import call
-
-for dist in get_installed_distributions():
-    call("pip2 install --upgrade --user " + dist.project_name, shell=True)
--- pip3-upgrade.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from subprocess import call
-from pip._internal.utils.misc import get_installed_distributions
-
-for dist in get_installed_distributions():
-    call("pip3 install --upgrade " + dist.project_name, shell=True)
