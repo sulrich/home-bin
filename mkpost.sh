b0386f0e40526f8d171bcdc53b1a02ec48977c83
steve ulrich <sulrich@botwerks.org>
2024-12-06 12:19:33 -0600

    updates and cleanup on mkpost.sh

--- mkpost.sh
+++ mkpost.sh
@@ -14,14 +14,12 @@ YEAR=$(date +"%Y")
 function print_usage() {
   cat <<EOF
 
-mkpost.sh (-c|--city "city" | -a|--auto-locate) -t--type [post|til]
+mkpost.sh -t|--type [post|til|link]
 
-  generate post using a city provided on the command line with the -c arg or if
-  run on a mac os system, use the CoreLocateCLI command to determine the
-  location and populate the interesting fields in the template.
+if a type has been provided (default is "post") the appropriate template and
+posting directory will be used. if the type is something other than "link" the
+script will prompt for a list of comma separated tags to fold into the mix.
 
-  if a type has been provided (default is "post") the appropriate template and 
-  posting directory will be used.
 EOF
 
 }
@@ -35,14 +33,6 @@ fi
 # handle command line args
 while [ "$1" != "" ]; do
   case $1 in
-    -c | --city )
-      shift
-      ARG_CITY="$1"
-      ;;
-    -a | --auto-locate )
-      shift
-      ARG_CITY=""
-      ;;
     -t | --type)
       shift
       ARG_TYPE="$1"
@@ -54,25 +44,15 @@ while [ "$1" != "" ]; do
   shift
 done
 
-if [ "$ARG_CITY" != "" ]
-then
-  CITY=${ARG_CITY}
-  LOCATION=${ARG_CITY}
-else
-  LOCATION_DATA=$(eval '/usr/bin/shortcuts run getCoreLocationData | cat')
-  CITY=$(jq -r '.city' <<<"${LOCATION_DATA}")
-  STATE=$(jq -r '.state' <<<"${LOCATION_DATA}")
-  LOCATION="${CITY}, ${STATE}"
-fi
-
-WEATHER=$(curl -s "http://wttr.in/${CITY}?format=+%c(%C)+%t(%f)+")
-LOCATION=$(echo "${LOCATION}" | tr '[:upper:]' '[:lower:]')
-
-
 if [ "${ARG_TYPE}" == "til" ]
 then
   TEMPLATE="${HOME}/.home/templates/markdown/blog-til.md"
   PAGE_DIR="til"
+elif [ "${ARG_TYPE}" == "link" ]
+then
+  TEMPLATE="${HOME}/.home/templates/markdown/blog-links.md"
+  PAGE_DIR="links"
+  DATELINK=$(date +"%d-%b-%Y")
 else
   TEMPLATE="${HOME}/.home/templates/markdown/blog-post.md"
   PAGE_DIR="posts"
@@ -87,8 +67,11 @@ echo -n "post title: "
 read -r POST_TITLE
 
 # get tags
-echo -n "post tags (comma separated): "
-read -r TAGS
+if [ "${ARG_TYPE}" != "link" ]
+then
+  echo -n "post tags (comma separated): "
+  read -r TAGS
+fi
 
 #  title cleanup                    | non-printable
 # FILE_TITLE=$(echo "${POST_TITLE}" | tr -dc "[:print:]" | \
@@ -111,6 +94,7 @@ else
   sed "s/%%TITLE%%/${POST_TITLE}/g" < "${TEMPLATE}"                     |\
   sed "s/%%WEATHER%%/${WEATHER}/g" | sed "s/%%LOCATION%%/${LOCATION}/g" |\
   sed "s/%%YEAR%%/${YEAR}/g" | sed "s/%%TIMESTAMP%%/${TIMESTAMP}/g"     |\
+  sed "s/%%TAGYEAR%%/${YEAR}/g" | sed "s/%%DATELINK%%/${DATELINK}/g"    |\
   sed "s/%%TAGS%%/${TAGS}/g" >> "${POST_FILE}"
   echo "editing: ${POST_FILE}"
   ${VISUAL} "${POST_FILE}"
