cdc24575a24beb82acc94aa74588692bff6cfb3e
steve ulrich <sulrich@botwerks.org>
2024-12-11 18:23:45 -0600

    updated mkpost script with some additional checks and variables

--- mkpost.sh
+++ mkpost.sh
@@ -6,10 +6,21 @@
 # appropriate for the host this script is being run on.
 #
 
+# validate required environment variables
+if [[ -z "${HUGO_DIR:-}" ]]; then
+    echo "ERROR: HUGO_DIR environment variable is not set" >&2
+    exit 1
+fi
+
+if [[ -z "${VISUAL:-}" ]]; then
+    echo "ERROR: VISUAL environment variable is not set" >&2
+    exit 1
+fi
+
 # post timestamp sample: date: "2014-04-14 10:15:53 -0500"
-TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S %z")
-DATESTAMP=$(date +"%Y%m%d-%H%M%S")
-YEAR=$(date +"%Y")
+readonly TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S %z")
+readonly DATESTAMP=$(date +"%Y%m%d-%H%M%S")
+readonly YEAR=$(date +"%Y")
 
 function print_usage() {
   cat <<EOF
@@ -22,6 +33,12 @@ script will prompt for a list of comma separated tags to fold into the mix.
 
 EOF
 
+}
+
+function validate_input() {
+    local input=$1
+    # remove any potentially dangerous characters
+    echo "$input" | tr -dc '[:alnum:][:space:]-_.,;()[]{}' || return 1
 }
 
 if [ $# -eq 0 ]
@@ -58,44 +75,67 @@ else
   PAGE_DIR="posts"
 fi
 
-# make sure that the local repo is current
-cd "${HUGO_DIR}" || exit
-# git pull
-
-# get the post title
+# get the post title with timeout
 echo -n "post title: "
-read -r POST_TITLE
+if ! read -r -t 60 POST_TITLE; then
+    echo "ERROR: Timeout waiting for post title input" >&2
+    exit 1
+fi
+
+# validate page directory
+if [[ ! "${PAGE_DIR}" =~ ^(til|links|posts)$ ]]; then
+    echo "ERROR: invalid page directory: ${PAGE_DIR}" >&2
+    exit 1
+fi
+
+# ensure the target directory exists
+if [[ ! -d "${HUGO_DIR}/content/${PAGE_DIR}" ]]; then
+    echo "ERROR: target directory does not exist: ${HUGO_DIR}/content/${PAGE_DIR}" >&2
+    exit 1
+fi
 
 # get tags
 if [ "${ARG_TYPE}" != "link" ]
 then
-  echo -n "post tags (comma separated): "
-  read -r TAGS
+    echo -n "post tags (comma separated): "
+    if ! read -r -t 60 TAGS; then
+        echo "ERROR: Timeout waiting for tags input" >&2
+        exit 1
+    fi
+    
+    # validate tags
+    TAGS=$(validate_input "$TAGS")
 fi
 
 #  title cleanup                    | non-printable
 # FILE_TITLE=$(echo "${POST_TITLE}" | tr -dc "[:print:]" | \
 #                # punctuation      | compress & replace spaces
 #                 tr -d "[:punct:]" | tr -s "[:space:]" "-")
-POST_FILE="${HUGO_DIR}/content/${PAGE_DIR}/${DATESTAMP}.md"
-echo "${POST_FILE}"
 
-# ------------------------------------------------------------------------------
-# check to make sure there isn't a post already. if there is, error out and
-# make the user fix it.  otherwise, do the necessary search and replace and
-# dump to a file in the right directory.
-#
-if [ -f "${POST_FILE}" ]; then
+readonly TEMP_FILE=$(mktemp)
+trap 'rm -f "${TEMP_FILE}"' EXIT
+
+readonly POST_FILE="${HUGO_DIR}/content/${PAGE_DIR}/${DATESTAMP}.md"
+echo "creating post file: ${POST_FILE}"
+if [ -f "${POST_FILE}" ]; 
+then
   echo "ERROR: POST FILE EXISTS - DEAL WITH THIS MANUALLY"
   echo "POST FILE: ${POST_FILE}"
   ${VISUAL} "${POST_FILE}"
   exit 1
 else
-  sed "s/%%TITLE%%/${POST_TITLE}/g" < "${TEMPLATE}"                     |\
-  sed "s/%%WEATHER%%/${WEATHER}/g" | sed "s/%%LOCATION%%/${LOCATION}/g" |\
-  sed "s/%%YEAR%%/${YEAR}/g" | sed "s/%%TIMESTAMP%%/${TIMESTAMP}/g"     |\
-  sed "s/%%TAGYEAR%%/${YEAR}/g" | sed "s/%%DATELINK%%/${DATELINK}/g"    |\
-  sed "s/%%TAGS%%/${TAGS}/g" >> "${POST_FILE}"
+  sed -e "s/%%TITLE%%/${POST_TITLE//\//\\/}/g" \
+    -e "s/%%WEATHER%%/${WEATHER:-}/g" \
+    -e "s/%%LOCATION%%/${LOCATION:-}/g" \
+    -e "s/%%YEAR%%/${YEAR}/g" \
+    -e "s/%%TIMESTAMP%%/${TIMESTAMP}/g" \
+    -e "s/%%TAGYEAR%%/${YEAR}/g" \
+    -e "s/%%DATELINK%%/${DATELINK:-}/g" \
+    -e "s/%%TAGS%%/${TAGS:-}/g" \
+    -e "s/%%HOSTNAME%%/${HOSTNAME:-}/g" \
+    < "${TEMPLATE}" > "${TEMP_FILE}"
+
+  mv "${TEMP_FILE}" "${POST_FILE}"
   echo "editing: ${POST_FILE}"
   ${VISUAL} "${POST_FILE}"
 fi
